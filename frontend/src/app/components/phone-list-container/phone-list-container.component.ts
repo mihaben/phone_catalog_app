bdee59a23667b4c894c2fc2a3e26ac85f5acf69e
Miguel Haba <miguelhaba@MacBook-Pro-de-Miguel.local>
2018-01-28 13:27:50 +0100

    PhoneList component working with ngrx and effects

--- frontend/src/app/components/phone-list-container/phone-list-container.component.ts
+++ frontend/src/app/components/phone-list-container/phone-list-container.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { PhonesService } from '../../services/phones.service';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import * as fromRoot from '../../store/reducer';
+import * as Actions from '../../store/actions';
+import { Action } from 'rxjs/scheduler/Action';
+
+import { Phone } from '../../models/phone.model';
 
 @Component({
   selector: 'app-phone-list-container',
   templateUrl: './phone-list-container.component.html',
   styleUrls: ['./phone-list-container.component.scss']
 })
+
 export class PhoneListContainerComponent implements OnInit {
 
-  loading: boolean;
-  phoneList: any[];
+  loading: Observable<Boolean>;
+  phoneList: Observable<Phone[]>;
 
-  constructor(private phonesService: PhonesService) {
-    this.loading = false;
+  constructor(private store: Store <fromRoot.State>) {
+    this.loading = this.store.pipe(select('phonesReducer')).pipe(select('loading'));
+    this.phoneList = this.store.pipe(select('phonesReducer')).pipe(select('results'));
   }
 
   ngOnInit() {
-    this.loadPhones();
-  }
-
-  loadPhones() {
-    this.loading = true;
-    this.phonesService.getPhones().subscribe(phones => {
-      console.log(phones);
-      this.loading = false;
-      this.phoneList = phones;
-    });
+    this.store.dispatch(new Actions.Load());
   }
 
 }
